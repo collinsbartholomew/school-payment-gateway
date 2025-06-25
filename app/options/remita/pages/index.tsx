86972d70af7af4cf2caba84cc20dec3398de1161
collinsbartholomew <152465316+collinsbartholomew@users.noreply.github.com>
2025-06-25 17:38:18 +0100

    Update index.tsx

--- app/options/remita/pages/index.tsx
+++ app/options/remita/pages/index.tsx
@@ -1,16 +1,16 @@
-import Head from 'next/head';
-import PaymentTabs from '../components/PaymentTabs';
+// import Head from 'next/head';
+// import PaymentTabs from '../components/PaymentTabs';
 
-export default function IndexPage() {
-	return (
-		<>
-			<Head>
-				<title>Remita Payments</title>
-				<meta name="description" content="Secure Remita payment integration" />
-			</Head>
-			<main className="min-h-screen flex items-center justify-center p-4">
-				<PaymentTabs />
-			</main>
-		</>
-	);
-}
+// export default function IndexPage() {
+// 	return (
+// 		<>
+// 			<Head>
+// 				<title>Remita Payments</title>
+// 				<meta name="description" content="Secure Remita payment integration" />
+// 			</Head>
+// 			<main className="min-h-screen flex items-center justify-center p-4">
+// 				<PaymentTabs />
+// 			</main>
+// 		</>
+// 	);
+// }
